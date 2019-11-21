1658aecdc4ecd4946f30ff931ed6a1e338537e06
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-11-21 13:16:39 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * unit-layout:/index.php
+ *
+ * @creation  2019-02-23
+ * @version   1.0
+ * @package   unit-layout
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+include(__DIR__.'/Layout.class.php');
